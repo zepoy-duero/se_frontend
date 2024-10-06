fbe0054467c1c6da37b69aaa4b8f5969b6d79c5d
ZEPOY DUERO <jepoyduero@gmail.com>
2024-10-06 10:11:00 +0800

    added sidenav component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <v-app>
+    <side-nav />
     <v-main>
-      <router-view>
-
-      </router-view>
+      <router-view> </router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
+import SideNav from "./components/SideNav.vue";
 export default {
-  name: 'App',
-  
+  components: { SideNav },
+  name: "App",
 };
 </script>
 
 <style>
-@import '@mdi/font/css/materialdesignicons.css';
-@import 'vuetify/styles';
+@import "@mdi/font/css/materialdesignicons.css";
+@import "vuetify/styles";
 </style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-navigation-drawer app permanent :mini-variant="mini_variant">
+    <v-list-item>
+      <v-avatar>
+        <v-img alt="John" src="@/assets/nemsu.jpg"></v-img>
+      </v-avatar>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list nav>
+      <v-list-item v-for="item in items" :key="item.title" link :to="item.url">
+        <template v-slot:prepend>
+          <v-icon :icon="item.icon"></v-icon>
+        </template>
+
+        <v-list-item-title v-text="item.title"></v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+  
+  <script>
+export default {
+  props: ["mini_variant"],
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard", url: "/dashboard" },
+        { title: "Students", icon: "mdi-account-multiple", url: "/" },
+        { title: "Prospectus", icon: "mdi-cart", url: "/prospectus" },
+
+        { title: "Documentation", icon: "mdi-help-box", url: "/documentation" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+  
+  <style></style>
