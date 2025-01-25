6a93c98b6db763974e65901550e06913b65e1c73
ZEPOY DUERO <jepoyduero@gmail.com>
2025-01-25 18:48:51 +0800

    list student table

--- src/components/ListStudents.vue
+++ src/components/ListStudents.vue
@@ -1,7 +1,9 @@
 <template>
   <v-container fluid>
     <v-card>
-      <v-card-title class="d-flex align-center pe-4">
+      <v-card-title class="text-h5"> Students List </v-card-title>
+      <v-divider></v-divider>
+      <v-card-title class="d-flex align-center pe-4 pt-4">
         <v-text-field
           v-model="params.params.search"
           density="compact"
@@ -11,71 +13,68 @@
           variant="solo-filled"
           flat
           single-line
-          hint="Type the name of student"
+          hide-details
         ></v-text-field>
+
         <v-spacer></v-spacer>
-        <v-btn
+        <v-icon class="mr-2" size="small" icon="mdi-download"></v-icon>
+        <v-icon class="mr-2" size="small" icon="mdi-printer-outline"></v-icon>
+        <v-icon
+          @click="createStudent"
+          class="mr-2"
+          size="small"
+          icon="mdi-plus"
+        ></v-icon>
+        <!-- <v-btn
+          class="align-center mt-0"
           size="small"
           rounded="xl"
           variant="flat"
           color="primary"
           @click="createStudent"
           >Add New</v-btn
-        >
+        > -->
       </v-card-title>
-      <v-data-table-server
-        :items-per-page="params.params.itemsPerPage"
-        :headers="headers"
-        :items="students"
-        :items-length="totalItems"
-        :loading="loading"
-        :search="params.params.search"
-        item-value="full_name"
-    @update:options="getStudents"
-      >
-        <template v-slot:[`item.full_name`]="{ item }">
-          <span>{{
-            item.last_name + ", " + item.first_name + " " + item.middle_name
-          }}</span>
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-row>
-            <v-col>
-              <v-icon
-                size="small"
-                color="blue"
-                @click="editStudent(item.student_id)"
+      <v-card-text>
+        <v-data-table-server
+          :row-props="itemColor"
+          :items-per-page="params.params.itemsPerPage"
+          :headers="headers"
+          :items="students"
+          :items-length="totalItems"
+          :loading="loading"
+          :search="params.params.search"
+          item-value="full_name"
+          @update:options="getStudents"
+        >
+          <template v-slot:[`item.profile_pic`]="{ item }">
+            <v-avatar v-if="item.profile_pic != ''">
+              <v-icon icon="mdi-account"></v-icon>
+            </v-avatar>
+            <v-avatar v-else>
+              <v-icon icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+          </template>
+          <template v-slot:[`item.full_name`]="{ item }">
+            <span>{{
+              item.last_name + ", " + item.first_name + " " + item.middle_name
+            }}</span>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <div class="text-end">
+              <v-icon color="blue" @click="editStudent(item.student_id)"
                 >mdi-pencil</v-icon
               >
-            </v-col>
-            <v-col>
               <v-icon
-                size="small"
+                class="mx-2"
                 color="red"
                 @click="deleteStudent(item.student_id)"
                 >mdi-delete</v-icon
               >
-            </v-col>
-            <v-col>
-              <v-icon
-                size="small"
-                color="green"
-                @click="evaluateStudent(item.student_id)"
-                >mdi-account-reactivate</v-icon
-              >
-            </v-col>
-          </v-row>
-        </template>
-        <template v-slot:top>
-          <v-pagination
-            v-model="params.params.page"
-          :length="
-            Math.ceil(totalItems / params.params.itemsPerPage)
-          "
-            @input="getStudents()"
-          ></v-pagination>
-        </template>
-      </v-data-table-server>
+            </div>
+          </template>
+        </v-data-table-server>
+      </v-card-text>
     </v-card>
 
     <!-- <StudentEvaluation :evalDialog="evalDialog" /> -->
@@ -105,6 +104,14 @@ export default {
       headers: [
         {
           align: "start",
+          title: "",
+          key: "profile_pic",
+          sortable: false,
+          loading: false,
+          class: "font-weight-black",
+        },
+        {
+          align: "start",
           title: "ID",
           key: "student_id",
           sortable: false,
@@ -122,7 +129,7 @@ export default {
         { title: "Course", key: "course" },
         { title: "Year Level", key: "year_level" },
         { title: "College Department", key: "college_department" },
-        { title: "Actions", key: "actions", align: "center" },
+        { title: "Actions", key: "actions", align: "end" },
       ],
     };
   },
@@ -130,9 +137,9 @@ export default {
     students() {
       return this.studentStore.students;
     },
-    totalItems(){
-      return Number(this.studentStore.totalItems)
-    }
+    totalItems() {
+      return Number(this.studentStore.totalItems);
+    },
   },
   created() {
     this.getStudents(this.params);
@@ -166,7 +173,12 @@ export default {
     evaluateStudent(studentId) {
       this.evalDialog = true;
     },
-     },
+    itemColor(item) {
+      if (item.item.id % 2 == 1) {
+        return { class: "bg-grey-lighten-4" };
+      }
+    },
+  },
 };
 </script>
 <style scoped>
